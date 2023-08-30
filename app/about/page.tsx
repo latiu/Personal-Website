3420cca3f5dd2ecd7f7f2cff2237be16856ff774
latiu <emily.nieu@gmail.com>
2023-08-30 15:55:09 -0400

    feat: basic tabs for about me page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,14 @@
+import AboutMeSelector from '@/components/aboutMeTabs'
+
 export default function About() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      about
+    <main className="flex p-24 h-[calc(100vh-72px)] items-center"> 
+      <AboutMeSelector
+        tab1="Art"
+        tab2="Dance"
+        tab3="Film"
+        title="ABOUT ME"
+      />
     </main>
   )
 }
---
+++ components/aboutMeTabs.tsx
@@ -0,0 +1,72 @@
+'use client'
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box className="p-6">
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  };
+}
+
+export default function aboutMeTabs(props:{tab1: String, tab2: String, tab3: String, title: String}) {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className="w-full h-full">
+      <Tabs value={value} onChange={handleChange} aria-label="tabs">
+        <Tab label={props.tab1} {...a11yProps(0)} className="bg-light-pink rounded-tl-lg"/>
+        <Tab label={props.tab2} {...a11yProps(1)} className="bg-light-pink"/>
+        <Tab label={props.tab3} {...a11yProps(2)} className="bg-light-pink rounded-tr-lg"/>
+        <div className="grid grid-cols-2 w-full">
+          <div className="font-anton text-5xl col-start-2 justify-self-end">{props.title}</div>
+        </div>
+      </Tabs>
+
+      <Box className="w-full h-[calc(100%-48px)] bg-white">
+        <TabPanel value={value} index={0}>
+          {props.tab1}
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          {props.tab2}
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          {props.tab3}
+        </TabPanel>
+      </Box>
+    </div>
+  );
+}
